c7a57be60f37eeb77e00b4ea8554d06a6f05f464
Kamil <jestem.kamil.wozniak@gmail.com>
2021-01-09 21:11:05 +0100

    feat: notes/subscription models

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -7,7 +7,7 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone', 'password')
+        fields = ('email', 'first_name', 'last_name', 'phone', 'address', 'password')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def update(self, instance, validated_data) -> User:
