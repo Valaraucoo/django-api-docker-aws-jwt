d88580c244e99d00c68f16aaffce7a5dd97fe8d1
Maciej Ładoś <macieklad@gmail.com>
2021-02-08 23:33:22 +0100

    chore: api subscription routes

--- notes/urls.py
+++ notes/urls.py
@@ -11,12 +11,14 @@ urlpatterns = [
 
     path('<int:pk>/', views.NoteRetrieveView.as_view(), name='notes-details'),
     path('<int:pk>/like/', views.LikeNoteView.as_view(), name='notes-like'),
-    path('<int:pk>/bookmark/', views.BookmarkNoteView.as_view(), name='notes-bookmark'),
-    path('<int:pk>/category/', views.CategoryNoteView.as_view(), name='notes-category'),
+    path('<int:pk>/bookmark/', views.BookmarkNoteView.as_view(),
+         name='notes-bookmark'),
+    path('<int:pk>/category/', views.CategoryNoteView.as_view(),
+         name='notes-category'),
 
     path('my-notes/', views.UserNotesListView.as_view(), name='my-notes'),
     path('likes/', views.UserLikesListView.as_view(), name='my-likes'),
     path('bookmarks/', views.UserBookmarksListView.as_view(), name='my-bookmarks'),
-    path('payment/', views.SubscriptionView.as_view(), name='payment'),
-    path('payment/notification/', views.PaymentNotificationView.as_view(), name='payment-notification'),
+    path('payment/notification/', views.PaymentNotificationView.as_view(),
+         name='payment-notification'),
 ]
